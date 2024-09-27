d619362bd586f39128f7580246ab6f1263c5be63
Cloud User <ec2-user@ip-172-31-30-103.eu-north-1.compute.internal>
2024-09-27 06:47:37 +0000

    commit

--- push_history.sh
+++ push_history.sh
@@ -1,5 +1,5 @@
 cd~/trainig-biz-Parva
-cat~/.bas-history/home/ec2-user//trainig-biz-Parva/history.txt
+cat~/.bash-history/home/ec2-user//trainig-biz-Parva/history.txt
 sync
 git add .
 git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
